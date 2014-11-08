67df4d99d3611ff50593313f499a24a8f705aa9c
rigidus <i.am.rigidus@gmail.com>
2014-11-08 05:38:30 +0300

    fix

--- src/moto.lisp
+++ src/moto.lisp
@@ -6,6 +6,6 @@
 ;; start
 (restas:start '#:moto :port 9997)
 (restas:debug-mode-on)
-;; (restas:debug-mode-off)
+;; (restas:debugg-mode-off)
 (setf hunchentoot:*catch-errors-p* t)
 ;; enter_point ends here
