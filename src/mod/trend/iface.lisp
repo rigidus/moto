53cc43c3438d6f4378bdedff8e94acb5e21548fd
rigidus <i.am.rigidus@gmail.com>
2014-11-26 06:56:36 +0300

    no export code to doc

--- src/mod/trend/iface.lisp
+++ src/mod/trend/iface.lisp
@@ -158,30 +158,6 @@ At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praese
                           :border 1))))))
   (:del (act-btn "DEL" (id i) "Удалить")
         (progn (del-flat (getf p :data)))))
-(in-package #:moto)
-
-;; (restas:define-route flat ("/flat/:flatid")
-;;   (with-wrapper
-;;     (let ((flat (get-flat 1)))
-;;       (trendtpl:flatpage
-;;        (list
-;;         :rooms (let ((r (rooms flat)))
-;;                  (cond ((equal 0 r) "Квартира-студия")
-;;                        ((equal 1 r) "1-комнатная квартира")
-;;                        ((equal 2 r) "2-комнатная квартира")
-;;                        ((equal 3 r) "3-комнатная квартира")
-;;                        ((equal 4 r) "4-комнатная квартира")
-;;                        (t (err "unknown rooms value"))))
-;;         :id (id flat)
-;;         :price (price flat)
-;;         :rooms (rooms flat)
-;;         :area_living (area-living flat)
-;;         :area_sum (area-sum flat)
-;;         :area_kitchen (area-kitchen flat)
-;;         :sanuzel (sanuzel flat)
-;;         :finishing (finishing flat)
-;;         :balcon (balcon flat)
-;;         )))))
 
 (in-package #:moto)
 
