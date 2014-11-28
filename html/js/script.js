3adea0b098468feb4d67eb88810b86e09b91f7e0
rigidus <i.am.rigidus@gmail.com>
2014-11-29 01:27:33 +0300

    new changes html

---
+++ html/js/script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+
+jQuery(document).ready(function(){
+var params = {
+		changedEl: "select",
+		visRows: 5,
+		scrollArrows: true
+	}
+	cuSel(params);
+});
+
+
+
+
+
+});
