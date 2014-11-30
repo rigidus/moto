4fda482f97b02c50075de74a99ba0ad7ef1af2c2
rigidus <i.am.rigidus@gmail.com>
2014-11-30 05:45:13 +0300

    hh export

--- src/mod/hh/entityes.lisp
+++ src/mod/hh/entityes.lisp
@@ -1,12 +1,12 @@
 ;; [[file:hh.org::*Сущности и автоматы][entity_and_automates]]
 (in-package #:moto)
 
-(define-entity search-profile "Сущность поисковые профили"
+(define-entity profile "Сущность поисковые профили"
   ((id serial)
    (user-id integer)
    (query varchar)
    (ts-create bigint)
    (ts-last bigint)))
 
-(make-search-profile-table)
+(make-profile-table)
 ;; entity_and_automates ends here
