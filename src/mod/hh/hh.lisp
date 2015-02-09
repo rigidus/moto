c126d26bad504cb65cfed539f1416bb940fecd78
rigidus <i.am.rigidus@gmail.com>
2015-02-09 10:28:20 +0300

    run-response

--- src/mod/hh/hh.lisp
+++ src/mod/hh/hh.lisp
@@ -439,7 +439,6 @@
 (in-package #:moto)
 
 
-
 ;; (setf drakma:*header-stream* *standard-output*)
 
 (defparameter *user-agent* "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0")
