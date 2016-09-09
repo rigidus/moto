c0f117927b172189cfb2853f2e9d0ee231a14f51
rigidus <avenger-f@yandex.ru>
2016-09-09 05:37:20 +0300

    cell building

--- dino/library.lisp
+++ dino/library.lisp
@@ -11,6 +11,80 @@
 (defun x-size ()
   (values 800 600))
 
+(defun surround ()
+  (floor
+   (multiple-value-bind (default-width default-height)
+       (x-size)
+     (floor (+ default-width default-height) 6))
+   *full-coefficient*))
+
+(defun image-plot (image x y &key red green blue)
+  (multiple-value-bind (default-width default-height)
+      (x-size)
+    (when (and (< x default-width) (< y default-height))
+      (when red
+        (setf (aref (zpng:data-array image) y x 0) red))
+      (when green
+        (setf (aref (zpng:data-array image) y x 1) green))
+      (when blue
+        (setf (aref (zpng:data-array image) y x 2) blue)))))
+
+(defun image-draw-line (image x1 y1 x2 y2 &key red green blue)
+  (declare (type integer x1 y1 x2 y2))
+  (let* ((dist-x (abs (- x1 x2)))
+         (dist-y (abs (- y1 y2)))
+         (steep (> dist-y dist-x)))
+    (when steep
+      (psetf x1 y1 y1 x1
+             x2 y2 y2 x2))
+    (when (> x1 x2)
+      (psetf x1 x2 x2 x1
+             y1 y2 y2 y1))
+    (let* ((delta-x (- x2 x1))
+           (delta-y (abs (- y1 y2)))
+           (var-error (floor delta-x 2))
+           (y-step (if (< y1 y2) 1 -1))
+           (y y1))
+      (loop
+         :for x :upfrom x1 :to x2
+         :do (if steep
+                 (image-plot image y x :red red :green green :blue blue)
+                 (image-plot image x y :red red :green green :blue blue)
+                 )
+         (setf var-error (- var-error delta-y))
+         (when (< var-error 0)
+           (incf y y-step)
+           (incf var-error delta-x))))
+    image))
+
+
+(defun get-nearest-point (h-x pnt-x pnt-y &key image)
+  (let ((nearest-points))
+    (loop :for item-x :from pnt-x :to (+ (surround) pnt-x) :do
+       (let ((nearest-y (car (sort
+                              (remove-if #'(lambda (z)
+                                             (or (< z pnt-y)
+                                                 (> z (+ pnt-y (surround)))))
+                                         (gethash item-x h-x))
+                              #'<))))
+         (unless (null nearest-y)
+           (setf nearest-points (append nearest-points (list (list item-x nearest-y))))
+           ))
+       (when image ;; dbg out img
+         (loop :for item-y :from pnt-y :to (+ (surround) pnt-y) :do
+            (image-plot image item-x item-y :blue 255)))
+       )
+    (setf nearest-points (cdr nearest-points))
+    (setf nearest-points (loop :for (nearest-x nearest-y) :in nearest-points :collect
+                            (let* ((dist-x  (- nearest-x pnt-x))
+                                   (dist-y  (- nearest-y pnt-y))
+                                   (quattro (+ (* dist-x dist-x) (* dist-y dist-y))))
+                              (when image
+                                (image-draw-line image pnt-x pnt-y nearest-x nearest-y :green 255))
+                              (list nearest-x nearest-y quattro))))
+    (car (sort nearest-points #'(lambda (a b) (< (nth 2 a) (nth 2 b)))))))
+
+
 (multiple-value-bind (default-width default-height) (x-size)
   (let ((y-rnd-cnt (floor default-height *full-coefficient*))
         (x-rnd-cnt (floor default-width  *full-coefficient*)))
@@ -19,17 +93,29 @@
              (raw-image->png
               (get-raw-image w :x 0 :y 0 :width default-width :height default-height :format :z-pixmap)
               default-width default-height))
-            (starters ())
-            (y-array (make-array (+ 1 y-rnd-cnt) :element-type `(integer 0 ,(+ 1 x-rnd-cnt)) :initial-element 0))
-            (x-array (make-array (+ 1 x-rnd-cnt) :element-type `(integer 0 ,(+ 1 y-rnd-cnt)) :initial-element 0)))
+            (starters ()))
         (loop :for y-idx :from 0 :to y-rnd-cnt :do
            (loop :for x-idx :from 0 :to x-rnd-cnt :do
-              (let ((random-y (random default-height))
-                    (random-x (random default-width)))
+              (let ((random-y (random (- default-height 1)))
+                    (random-x (random (- default-width 1))))
                 (push (list random-x random-y) starters))))
-        (loop :for (x y) :in starters :do
-           (setf (aref (zpng:data-array image) y x 0) 255))
-        (zpng:write-png image "cell.png")
-        (sort starters #'(lambda (a b)
-                           (> (cadr a) (cadr b))))
-        ))))
+        (let ((h-x (make-hash-table)))
+          (loop :for (x y) :in starters :do
+             (if (null (gethash x h-x))
+                 (setf (gethash x h-x) (list y))
+                 (setf (gethash x h-x) (append (list y) (gethash x h-x)))))
+          ;; (maphash #'(lambda (k v)
+          ;;              (print (list k v)))
+          ;;          h-x)
+          (loop :for (pnt-x pnt-y) :in starters :do
+             (image-plot image pnt-x pnt-y :red 255 :green 0 :blue 0))
+          ;; get random point
+          (loop :repeat 200 :do
+             (destructuring-bind (pnt-x pnt-y)
+                 (nth (random (length starters)) starters)
+               (print (list :random-point pnt-x pnt-y))
+               (image-plot image pnt-x pnt-y :red 255 :green 255 :blue 255)
+               (print (get-nearest-point h-x pnt-x pnt-y :image image))
+               )))
+        ;; write png
+        (zpng:write-png image "cell.png")))))
