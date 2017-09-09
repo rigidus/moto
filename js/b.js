2cf2d2e5b970f65560664b6000ab7e2cfe53940c
rigidus <i.am.rigidus@gmail.com>
2017-09-09 07:51:55 +0300

    3 cols with toggle

--- js/b.js
+++ js/b.js
@@ -1,12 +1,19 @@
-function showlegend () {
-  $(".legend").show();
-  $(".hidelegend").show();
-  $(".showlegend").hide();
-
-}
-
-function hidelegend () {
-  $(".legend").hide();
-  $(".hidelegend").hide();
-  $(".showlegend").show();
+function tggl (on, off, ctrl, me) {
+  if ($(ctrl).is(":hidden")) {
+    setTimeout(function(){
+      $(me).text(off);
+      $(me).removeClass("btn-info");
+      $(me).addClass("btn-primary");
+      $(ctrl).show();
+    }, 200);
+  }
+  if ($(ctrl).is(":visible")) {
+    setTimeout(function(){
+      $(me).text(on);
+      $(me).removeClass("btn-primary");
+      $(me).addClass("btn-info");
+      $(ctrl).hide();
+    }, 200);
+  }
+  return false;
 }
